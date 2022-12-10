3ff64076cd3da123ebf1191be989118bd0b82aab
dev-ashanaur <devashanaur009@gmail.com>
2022-12-11 04:15:35 +0600

    created todo and clearall todo , complete all todo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,37 @@
 import notesImage from '../assets/images/notes.png';
 import doubleTickImage from '../assets/images/double-tick.png';
 import plusImage from '../assets/images/plus.png';
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+import { added,allCompleted,clearCompleted } from '../redux/todos/actions';
 const Header = () => {
+    const dispatch = useDispatch();
+    const [input,setInput] = useState("");
+
+    const handleInput = (e) =>{
+        setInput(e.target.value);
+    }
+
+    const submitHandler = (e)=>{
+        e.preventDefault();
+        if(input){
+            dispatch(added(input));
+            setInput("");
+        }
+    }
+
+    const completeHandler = () =>{
+        dispatch(allCompleted());
+    }
+
+    const clearHandler = () =>{
+        dispatch(clearCompleted());
+    }
+
     return (
         <div>
             <form
-                className="flex items-center bg-gray-100 px-4 py-4 rounded-md"
+                className="flex items-center bg-gray-100 px-4 py-4 rounded-md" onSubmit={submitHandler}
             >
                 <img
                     src={notesImage}
@@ -15,6 +41,8 @@ const Header = () => {
                 <input
                     type="text"
                     placeholder="Type your todo"
+                    onChange={handleInput}
+                    value={input}
                     className="w-full text-lg px-4 py-1 border-none outline-none bg-gray-100 text-gray-500"
                 />
                 <button
@@ -24,7 +52,7 @@ const Header = () => {
             </form>
 
             <ul className="flex justify-between my-4 text-xs text-gray-500">
-                <li className="flex space-x-1 cursor-pointer">
+                <li className="flex space-x-1 cursor-pointer" onClick={completeHandler}>
                     <img
                         className="w-4 h-4"
                         src={doubleTickImage}
@@ -32,7 +60,7 @@ const Header = () => {
                     />
                     <span>Complete All Tasks</span>
                 </li>
-                <li className="cursor-pointer">Clear completed</li>
+                <li className="cursor-pointer" onClick={clearHandler}>Clear completed</li>
             </ul>
         </div>
     )
--- src/redux/todos/reducer.js
+++ src/redux/todos/reducer.js
@@ -17,38 +17,38 @@ const reducer = (state = initialState, action) => {
                 },
             ];
         case TOGGLED:
-            return state.map((todo)=>{
-                if(todo.id !== action.payload){
+            return state.map((todo) => {
+                if (todo.id !== action.payload) {
                     return todo;
                 }
 
                 return {
                     ...todo,
-                    completed : !todo.completed
+                    completed: !todo.completed
                 }
             });
         case COLORSELECTED:
-            const {color,todoId} = action.payload;
-            return state.map((todo)=>{
-                if(todo.id !== todoId){
+            const { color, todoId } = action.payload;
+            return state.map((todo) => {
+                if (todo.id !== todoId) {
                     return todo;
                 }
                 return {
                     ...todo,
-                    color : color,
+                    color: color,
                 }
             });
         case DELETED:
-            return state.filter((todo)=>todo.id !== action.payload);
+            return state.filter((todo) => todo.id !== action.payload);
         case ALLCOMPLETED:
-            return state.map((todo)=>{
+            return state.map((todo) => {
                 return {
-                    ...state,
+                    ...todo,
                     completed: true
                 }
             })
         case CLEARCOMPLETED:
-            return state.filter((todo)=>!todo.completed);
+            return state.filter((todo) => !todo.completed);
         default:
             return state;
     }
